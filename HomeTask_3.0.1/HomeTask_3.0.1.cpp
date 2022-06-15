93a3d3a8f33e851cac99cfb224f465cf325d7e34
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-15 11:25:29 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_3.0.1/HomeTask_3.0.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int a, b;
+    cout << "Введите два числа: \n";
+    cin >> a >> b;
+    cout << "Первое число делится без остатка на: \n";
+    for (int i = 1; i <= a; i++) {
+        if (a % i == 0) {
+            cout << i << endl;
+        }
+
+    }
+    cout << "Второе число делится без остатка на: \n";
+    for (int i = 1; i <= b; i++) {
+        if (b % i == 0) {
+            cout << i << endl;
+        }
+    }
+}
